ba9af20cd16ea429aa3f6cd3e2707881fa9cdcef
Sulogno Sarkar <rhitsarkar2003@gmail.com>
2025-07-25 11:06:32 +0530

    variablr

---
+++ variable/index.js
@@ -0,0 +1,16 @@
+// let x = 10;
+// let y;
+// y = 20;
+// window.alert(`hello = ${x+y}`);
+// console.log(typeof x)
+//bool = true;
+
+//variable
+
+let name = "Sulogno Sarkar";
+let age = 21;
+let isstudent = true;
+
+document.getElementById("1").textContent =`My name is ${name}`; 
+document.getElementById("2").textContent = `My age is ${age}`;
+document.getElementById("3").textContent = `I am a student ${isstudent}`;
