aefdd62e5129ffe6440027bb98b80e9a00347a97
Sulogno Sarkar <rhitsarkar2003@gmail.com>
2025-07-25 10:35:29 +0530

    basic

---
+++ day1/index.js
@@ -0,0 +1,9 @@
+// console.log("Hello")
+// window.alert("This is a alert box")
+//this is comment
+/*
+this is a comment
+ */
+
+document.getElementById("h1").textContent = `Hello`;
+document.getElementById("p").textContent = `my pizza`;
