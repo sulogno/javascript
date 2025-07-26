809e6d82b3f2d582b69776b57182c88fd5f3e84c
Sulogno Sarkar <rhitsarkar2003@gmail.com>
2025-07-26 18:03:21 +0530

    function

---
+++ 06_function/script.js
@@ -0,0 +1,11 @@
+function happybirthday() {
+    const heading = document.getElementById("h1");
+    heading.textContent = "Happy Birthday";
+
+    console.log("Happy birthday");
+    console.log("Happy birthday");
+    console.log("Happy birthday");
+    console.log("Happy birthday to you");
+}
+
+happybirthday();
